d26c1bd9c7a86f00fb8566667abb9b1b025a3fd7
asadmin <asadmin@Franklintn.gov>
2021-11-19 12:12:48 -0600

    Commit 11/19/2021

---
+++ models/Password.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const PasswordSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    unique: true,
+    trim: true,
+    maxlength: [50, 'Name can not be more than 50 characters']
+  },
+  slug: String,
+  username: {
+    type: String,
+    required: [true, 'Please add a username'],
+    trim: true
+  },
+  password: {
+    type: String,
+    required: [true, 'Please enter a password'],
+    trim: true
+  },
+  comments: {
+    type: String,
+    maxlength: [500, 'Comments max length is 500 characters']
+  },
+  website: {
+    type: String,
+    match: [
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/, 'Please use a valid URL with HTTP or HTTPS'
+    ]
+  }
+});
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-// Route files
-const passwords = require('./routes/passwords');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config( { path: './config/config.env' });
+
+// Connect to database
+connectDB();
+
+// Route files
+const passwords = require('./routes/passwords');
 
 const app = express();
 
@@ -20,5 +25,12 @@ app.use('/api/v1/passwords', passwords);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.bgGreen));
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.bgBrightRed);
+  // Close server and exit process
+  server.close(() => process.exit(1));
+});
 

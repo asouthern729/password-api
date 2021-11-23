deae679eeef199bf18c86abaa8bd271a9186a4ea
asadmin <asadmin@Franklintn.gov>
2021-11-23 16:26:04 -0600

    Added readme and started config 11/23/2021 16:26

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
@@ -31,11 +32,6 @@ app.use(express.json());
 // Cookie parser
 app.use(cookieParser());
 
-// Dev logging middleware
-if(process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-};
-
 // Sanitize data
 app.use(mongoSanitize());
 
@@ -58,6 +54,9 @@ app.use(hpp());
 
 // Enable cors
 app.use(cors());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Mount routers
 app.use('/api/v1/passwords', passwords);
